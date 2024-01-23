763e477881409ae1fac5a2e422e7aa8d3c1a9701
Luiz <lhenrique78155@gmail.com>
2024-01-22 22:30:42 -0300

    Feat < estilização > estilização do componente search, além de melhorias no mesmo e ajustes nos outros arquivos .css

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,14 +1,17 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import MainContext from '../../context/main';
 import { fetchIGBE } from '../../services/Api';
+import './index.css';
 
 function Search() {
   const { form, changeForm, changeApi } = useContext(MainContext);
+  const [status, setStatus] = useState<'show' | 'hide'>('hide');
+  const [inputData, setInputData] = useState('');
 
   const handleChange = (target: EventTarget & HTMLInputElement) => {
     const { name, value } = target;
     const newData = { filter: name, search: value };
-
+    setInputData(value);
     changeForm(newData);
   };
 
@@ -18,18 +21,30 @@ function Search() {
     changeApi(searchFetch);
   };
 
+  const changeStatus = () => {
+    if (status === 'show') setStatus('hide');
+    else setStatus('show');
+    setInputData('');
+  };
+
   return (
-    <>
-      <button className="search-btn">Pesquisar</button>
+    <div className="search-container">
+      <button
+        className="search-btn"
+        onClick={ changeStatus }
+      >
+        Pesquisar
+      </button>
       <form onSubmit={ (event) => handleSubmit(event) }>
         <input
           type="text"
           name="search"
-          className="search-input"
+          value={ inputData }
+          className={ `search-input ${status}` }
           onChange={ ({ target }) => handleChange(target) }
         />
       </form>
-    </>
+    </div>
   );
 }
 
